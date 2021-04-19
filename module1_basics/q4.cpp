cf9ef4fed4b65c90351f5460906daf2fc811491f
Himadri Bhavsar <77726296+himadri331@users.noreply.github.com>
2021-04-19 11:35:01 +0530

    Create q4.cpp

---
+++ module1_basics/q4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout<<"Enter any integer:"<<endl;
+    cin>>num;
+    if ( num%2==0)
+        cout<<"even"<<endl;
+    else#include <iostream>
+
+        cout<<"odd"<<endl;
+        
+    
+}
